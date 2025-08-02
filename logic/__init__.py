bf6e6e9384bc7a42d5af181719c111cdf28ea464
yuvalweil <75201490+yuvalweil@users.noreply.github.com>
2025-08-02 20:32:37 +0300

    Update __init__.py

--- logic/__init__.py
+++ logic/__init__.py
@@ -1,1 +1,13 @@
+from .core import assign_rooms
+from .validate import validate_constraints
+from .calendar_store import rebuild_calendar_from_assignments
+from .diagnostics import explain_soft_constraints
+from .utils import are_serial
 
+__all__ = [
+    "assign_rooms",
+    "validate_constraints",
+    "rebuild_calendar_from_assignments",
+    "explain_soft_constraints",
+    "are_serial",
+]
