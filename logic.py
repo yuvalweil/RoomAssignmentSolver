e7e3b0c7186f36291dcad7e125f63042ea9bb006
yuvalweil <75201490+yuvalweil@users.noreply.github.com>
2025-08-02 09:58:39 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -11,24 +11,24 @@ def assign_rooms(families_df, rooms_df):
     rooms = {
         row["id"]: {
             "type": row["type"],
-            "max_occupancy": row["max_occupancy"],
-            "bookings": []
+            "max_occupancy": row["max_occupancy"]
         } for _, row in rooms_df.iterrows()
     }
 
+    assigned_rooms = set()
     assignments = []
 
     for _, fam in families_df.iterrows():
-        check_in = datetime.strptime(fam["check_in"], "%d/%m/%Y")
-        check_out = datetime.strptime(fam["check_in"], "%d/%m/%Y")
+        check_in = datetime.strptime(fam["check_in"], "%Y-%m-%d")
+        check_out = datetime.strptime(fam["check_out"], "%Y-%m-%d")
 
         for room_id, room in rooms.items():
-            if (room["type"] == fam["room_type"] and
-                room["max_occupancy"] >= fam["people"] and
-                is_available(room["bookings"], check_in, check_out)):
-
-                room["bookings"].append({"check_in": check_in, "check_out": check_out})
+            if (room_id not in assigned_rooms and
+                room["type"] == fam["room_type"] and
+                room["max_occupancy"] >= fam["people"]):
+                
                 assignments.append({"family": fam["id"], "room": room_id})
+                assigned_rooms.add(room_id)
                 break
 
     return pd.DataFrame(assignments)
