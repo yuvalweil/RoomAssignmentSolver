b405c302e6fb370b64ec6f51d7434fcf6aab7771
yuvalweil <75201490+yuvalweil@users.noreply.github.com>
2025-08-05 17:14:50 +0300

    Update __init__.py

--- logic/__init__.py
+++ logic/__init__.py
@@ -1,10 +1,15 @@
 # logic/__init__.py
-
-from .solver   import assign_rooms, assign_per_type
+from .solver import assign_rooms, assign_per_type   # <- use the new solver
 from .validate import validate_constraints
+from .calendar_store import rebuild_calendar_from_assignments
+from .diagnostics import explain_soft_constraints
+from .utils import are_serial
 
 __all__ = [
     "assign_rooms",
     "assign_per_type",
     "validate_constraints",
+    "rebuild_calendar_from_assignments",
+    "explain_soft_constraints",
+    "are_serial",
 ]
